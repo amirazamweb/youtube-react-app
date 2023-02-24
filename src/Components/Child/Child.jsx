fb79d109c7e70353905e8ef1b54924cbf6f7b382
amir azam <amirazam281@gmail.com>
2023-02-25 02:32:43 +0530

    commits

--- src/Components/Child/Child.jsx
+++ src/Components/Child/Child.jsx
@@ -5,7 +5,7 @@ const Child = (props) => {
         <div className={style.unit}>
             <iframe src={`https://www.youtube.com/embed/${props.videoId}`} title="YouTube video player" frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
+                allowFullScreen className={style.iframe}></iframe>
             <p className={style.title}>{props.description}</p>
             <p className={style.channelTitle}>{props.channelTitle}</p>
         </div>
--- src/Components/Header/Header1/Header1.jsx
+++ src/Components/Header/Header1/Header1.jsx
@@ -4,7 +4,7 @@ import style from './Header1.module.css'
 const Header1 = () => {
     return (
         <div className={style.header1}>
-            <i className="fa-solid fa-bars"></i>
+            <i className={`fa-solid fa-bars ${style.sideIcon}`}></i>
             <img src={logo} alt="Youtube" />
         </div>
     )
